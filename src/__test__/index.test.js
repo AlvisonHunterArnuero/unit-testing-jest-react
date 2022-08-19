7ed9cb7da88f356caca77b3736acd6f4a6bae6f7
Alvison Hunter <alvison@gmail.com>
2022-08-18 23:17:50 -0600

    Updated files with a better formatting

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,10 +1,11 @@
 const rndLanguage = require("../index");
 describe("Test Random Language Functionalities & language existence", () => {
   test("should test random language functionality ", () => {
-    expect(typeof rndLanguage()).toBe('string');
+    expect(typeof rndLanguage()).toBe("string");
   });
 
   test("Check if a language exists", () => {
-    expect(rndLanguage()).not.toContain('Go');
+    expect(rndLanguage()).not.toMatch("Go");
   });
+  
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const languages = ["Java", "JavaScript", "Python", "C++", "PHP"];
+const languages = [
+  "Java",
+  "JavaScript",
+  "Python",
+  "C++",
+  "PHP",
+  "Fortran",
+  "Clipper",
+  "Haskel",
+];
 
-const rndLanguage = () => languages[Math.floor(Math.random() * languages.length)];
+const rndLanguage = () =>
+  languages[Math.floor(Math.random() * languages.length)];
 
 module.exports = rndLanguage;
